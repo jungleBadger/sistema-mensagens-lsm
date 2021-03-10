56906267c48994c2955057115b5e9e472c022552
Daniel Abrao <danielpaladar@gmail.com>
2021-03-10 00:13:54 -0300

    Add initial server files

---
+++ server/src/server.ts
@@ -0,0 +1,68 @@
+"use strict";
+
+// @ts-ignore
+import compression = require("compression");
+import cookieParser = require("cookie-parser");
+
+import debug from "debug";
+import express = require("express");
+import helmet = require("helmet");
+import morgan = require("morgan");
+import * as fs from "fs";
+
+const app: express.Application = express();
+const APP_PORT: number|string = process.env.APP_PORT || 3030;
+const log: any = debug("app:main");
+const httpLog: any = debug("app:endpoint");
+let server: express.Application;
+
+app.use(compression());
+app.use(cookieParser(process.env.APP_SECRET));
+
+app.use(helmet({
+    "contentSecurityPolicy": false
+}));
+app.use(express.urlencoded({
+    "extended": true,
+    "limit": "3mb"
+}));
+app.use(express.json());
+
+if (httpLog.enabled) {
+    app.use(
+        morgan(
+            "combined",
+            {
+                "stream": {
+                    "write": (msg: string) => httpLog(msg.trimEnd())
+                }
+            }
+        )
+    );
+}
+
+
+export async function run (CUSTOM_APP_PORT: number = 0) {
+
+    if (process.env.LOCAL_HTTPS) {
+        server = require("https").createServer({
+            "hostname": "localhost",
+            "agent": false,
+            "key": fs.readFileSync("../../root/certificates/local/localhost-privkey.pem"),
+            "cert": fs.readFileSync("../../root/certificates/local/localhost-cert.pem"),
+            "rejectUnauthorized": false
+        } as object, app);
+    } else {
+        server = require("http").createServer(app);
+    }
+
+    log(`${process.env.LOCAL_HTTPS ? "HTTPS" : "HTTP"} server created`);
+
+    app.get("/", (req, res) => res.status(200).send("xx"))
+
+    server.listen(CUSTOM_APP_PORT || APP_PORT, function () {
+        log(`Server started at port: ${CUSTOM_APP_PORT || APP_PORT}`);
+    });
+
+    return server;
+}
