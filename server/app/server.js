03ad79955e26675c6c5abadffaf559217b860e90
Daniel Abrao <danielpaladar@gmail.com>
2021-03-27 10:12:02 -0300

    Add initial circleCI integration

--- server/app/server.js
+++ server/app/server.js
@@ -18,6 +18,13 @@ const APP_PORT = process.env.APP_PORT || 3030;
 const log = debug("app:main");
 const httpLog = debug("app:endpoint");
 
+
+const DBConnectionPool = require("./helpers/DBConnectionPool");
+
+let connectionPool = new DBConnectionPool(`DATABASE=lsmdb;HOSTNAME=localhost;PORT=50000;PROTOCOL=TCPIP;UID=db2inst1;PWD=test;CURRENTSCHEMA=DB2INST1`)
+
+
+
 let server;
 
 app.use(compression());
@@ -56,6 +63,19 @@ app.use("/docs/api", express.static(path.join("..", "/docs/api/swagger-ui-dist")
 log("Static routes loaded");
 
 module.exports = async function run (CUSTOM_APP_PORT = 0) {
+
+
+
+
+	// let result1 = await connectionPool.executeRawSqlInstruction("SELECT * FROM TEST");
+	// let result2 = await connectionPool.executeRawSqlInstruction("INSERT INTO TEST (ID) VALUES 55");
+	//
+	// LIMIT 10
+	// let result3 = await connectionPool.executePreparedSqlInstruction("INSERT INTO TEST (ID) VALUES (?)", [1]);
+	let result4 = await connectionPool.executePreparedSqlInstruction(" SELECT * FROM TEST WHERE ID = ? LIMIT 10", [55]);
+
+	console.log(result4);
+
 	if (process.env.LOCAL_HTTPS) {
 		server = https.createServer({
 			"hostname": "localhost",
