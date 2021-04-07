5ee8799881fd327b3185a40f29442e09321a0b3f
Daniel Abrao <danielpaladar@gmail.com>
2021-04-06 23:21:43 -0300

    Add initial vue components

---
+++ client/_etc/shared_components/lsm-header.vue
@@ -0,0 +1,37 @@
+<template>
+	<header id="lsm-header">
+		<slot name="app-menu">
+
+		</slot>
+		<slot name="app-title">
+			<h2>LSM App</h2>
+		</slot>
+	</header>
+</template>
+<script type="text/javascript">
+
+
+"use strict";
+import { defineComponent } from "vue";
+import { useI18n } from "vue-i18n";
+
+export default defineComponent({
+	"name": "LsmSideMenu",
+	setup() {
+		return {
+			...useI18n()
+		}
+	}
+});
+</script>
+<style scoped lang="scss" rel="stylesheet/scss">
+
+@import "../css/variables.scss";
+
+#lsm-header {
+	width: 100%;
+	height: 48px;
+	background-color: $primary-dark-color;
+}
+
+</style>
