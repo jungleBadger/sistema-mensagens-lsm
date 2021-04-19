6a92ea2f6f6b8591bf3400aa241f006e6b25d22d
Daniel Abrao <danielpaladar@gmail.com>
2021-04-18 22:15:04 -0300

    Add initial input and button components

---
+++ client/_etc/shared_components/ui/lsm-button.vue
@@ -0,0 +1,27 @@
+<template>
+	<button
+		class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+		type="submit">
+		<span class="absolute left-0 inset-y-0 flex items-center pl-3">
+			<font-awesome-icon :icon="['fas', 'lock']"></font-awesome-icon>
+		</span>
+		<span>Sign in</span>
+	</button>
+</template>
+<script>
+
+export default {
+	"name": "LsmButton",
+
+	"props": {
+
+		"label": {
+			"type": String,
+			"required": false,
+			"default": function () {
+				return "";
+			}
+		}
+	}
+}
+</script>
---
+++ client/_etc/shared_components/ui/lsm-input.vue
@@ -0,0 +1,57 @@
+<template>
+
+	<div
+		:key="id">
+		<label
+			class="sr-only"
+			:for="id">
+			{{label}}
+		</label>
+		<input
+			class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+			:class="extraInputClasses"
+			:id="id"
+			v-bind="$attrs"
+			:value="modelValue"
+			@input="(event) => $emit('update:modelValue', event.target.value)"
+		/>
+	</div>
+
+</template>
+<script>
+
+
+import { v4 as uuidv4 } from 'uuid';
+
+export default {
+	"name": "LsmInput",
+	"inheritAttrs": false,
+
+	"props": {
+
+		"modelValue": {
+			"type": String,
+			"required": false,
+			"default": function () {
+				return "";
+			}
+		},
+
+		"label": {
+			"type": String,
+			"required": false,
+			"default": function () {
+				return "";
+			}
+		},
+
+		"id": {
+			"type": String,
+			"required": false,
+			"default": function () {
+				return uuidv4();
+			}
+		}
+	}
+}
+</script>
