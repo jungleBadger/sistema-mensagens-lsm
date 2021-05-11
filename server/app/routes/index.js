6e35db02e184f1d8f19bcc475e63fa799e3465a4
Daniel Abrao <danielpaladar@gmail.com>
2021-05-11 14:53:43 -0300

    Add google verification strategy

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -18,9 +18,14 @@ module.exports = function (app) {
         return res.send(swaggerJSDoc);
     });
 
+    app.get(["/google4aa023a715102fc5", "google4aa023a715102fc5.html"], (req, res) => {
+    	return res.status(200).render("./_etc/google4aa023a715102fc5.html");
+	});
+
     app.get("/", (req, res) => {
     	return res.redirect("/app");
 	});
+
 
     app.use(
     	adminHandler
