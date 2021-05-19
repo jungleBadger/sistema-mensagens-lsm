f6260558a8b15d49f6e8e742d60e8ca81df709ac
Daniel Abrao <danielpaladar@gmail.com>
2021-05-18 23:44:51 -0300

    Add `events` actions and state

--- client/admin_module/src/js/store/_events/actions.js
+++ client/admin_module/src/js/store/_events/actions.js
@@ -1,6 +1,212 @@
 "use strict";
 
+import eventsFactory from "../../factory/events";
+
 export default {
 
+	async createEvent(context, title) {
+		try {
+			await eventsFactory.createEvent({title});
+			context.commit(
+				"notification/addNotification",
+				{
+					"kind": "success",
+					"title": "Sucesso!",
+					"subtitle": "Evento criado com sucesso."
+				},
+				{"root": true}
+			);
+			return true;
+		} catch (e) {
+			let error = {
+				"title": "Houve um erro ao criar o Evento.",
+				"subtitle": "Confira os dados, tente novamente e se o erro persistir contate o suporte."
+			}
+
+			if (e.status === 409) {
+				error = {
+					"title": "Houve um conflito ao criar o Evento.",
+					"subtitle": `Evento ${title} já existe no sistema.`
+				}
+			}
+
+			context.commit(
+				"notification/addNotification",
+				{
+					"kind": "error",
+					"title": error.title,
+					"subtitle": error.subtitle
+				},
+				{"root": true}
+			);
+			console.log(e);
+			return false;
+		}
+	},
+
+	async searchEvents(context, params) {
+		const {skip, limit, orderBy, orderDirection} = context.getters["pagination"];
+		try {
+			let result = await eventsFactory.searchEvents(params.filterText, params.filterColumn, skip, limit, orderBy, orderDirection);
+			context.commit("totalEventsCount", result.totalCount);
+			context.commit("eventItems", result.results);
+		} catch (e) {
+			context.commit(
+				"notification/addNotification",
+				{
+					"kind": "error",
+					"title": "Houve um erro realizando a busca.",
+					"subtitle": "Confira os filtros, tente novamente e se o erro persistir contate o suporte."
+				},
+				{"root": true}
+			);
+		}
+
+	},
+
+	async retrieveTotalEventsCount(context) {
+		try {
+			let result = await eventsFactory.retrieveTotalEventsCount();
+			context.commit("totalEventsCount", result.count);
+		} catch (e) {
+			context.commit(
+				"notification/addNotification",
+				{
+					"kind": "error",
+					"title": "Houve um erro calculando o total de items.",
+					"subtitle": "Confira os filtros, tente novamente e se o erro persistir contate o suporte."
+				},
+				{"root": true}
+			);
+		}
+
+	},
+
+	async retrieveEvents(context) {
+		const {skip, limit, orderBy, orderDirection} = context.getters["pagination"];
+
+		try {
+			let events = await eventsFactory.retrieveEvents(skip, limit, orderBy, orderDirection);
+			context.commit("eventItems", events.results);
+		} catch (e) {
+			context.commit(
+				"notification/addNotification",
+				{
+					"kind": "error",
+					"title": "Houve um erro buscando dados.",
+					"subtitle": "Confira os filtros, tente novamente e se o erro persistir contate o suporte."
+				},
+				{"root": true}
+			);
+		}
+
+	},
+
+	async retrieveEventById(context, eventId) {
+
+		try {
+			return await eventsFactory.retrieveEventById(eventId);
+
+		} catch (e) {
+			context.commit(
+				"notification/addNotification",
+				{
+					"kind": "error",
+					"title": `Houve um erro ao carregar o Evento ${eventId}.`,
+					"subtitle": "Confira os dados, tente novamente e se o erro persistir contate o suporte."
+				},
+				{"root": true}
+			);
+			console.log(e);
+			return false;
+		}
+
+
+	},
+
+
+	async updateEvent(context, event) {
+		try {
+			await eventsFactory.updateEvent(event);
+			let events = context.getters["eventItems"];
+
+			context.commit(
+				"notification/addNotification",
+				{
+					"kind": "success",
+					"title": "Sucesso!",
+					"subtitle": "Evento atualizado com sucesso."
+				},
+				{"root": true}
+			);
+			context.commit(
+				"eventItems",
+				events.map(item => {
+					return item.id === event.id ? {
+						...item,
+						...event
+					} : item;
+				})
+			);
+			return true;
+		} catch (e) {
+			let error = {
+				"title": "Houve um erro atualizando o Evento.",
+				"subtitle": "Confira os dados, tente novamente e se o erro persistir contate o suporte."
+			}
+
+			if (e.status === 409) {
+				error = {
+					"title": "Houve um conflito ao atualizar o Evento.",
+					"subtitle": `Evento ${event.title} já existe no sistema.`
+				}
+			}
+
+			context.commit(
+				"notification/addNotification",
+				{
+					"kind": "error",
+					"title": error.title,
+					"subtitle": error.subtitle
+				},
+				{"root": true}
+			);
+			console.log(e);
+			return false;
+		}
+	},
+
+	async deleteEvent(context, eventId) {
+		try {
+			await eventsFactory.deleteEvent(eventId);
+			let events = context.getters["eventItems"];
+
+			context.commit(
+				"notification/addNotification",
+				{
+					"kind": "success",
+					"title": "Sucesso!",
+					"subtitle": "Evento excluído com sucesso."
+				},
+				{"root": true}
+			);
+			context.commit("totalEventsCount", events.length - 1);
+			context.commit("eventItems", events.filter(item => item.id !== eventId));
+			return true;
+		} catch (e) {
+			context.commit(
+				"notification/addNotification",
+				{
+					"kind": "error",
+					"title": "Houve um erro excluindo o Evento.",
+					"subtitle": "Confira os dados, tente novamente e se o erro persistir contate o suporte."
+				},
+				{"root": true}
+			);
+			console.log(e);
+			return false;
+		}
+
+	}
 
 };
