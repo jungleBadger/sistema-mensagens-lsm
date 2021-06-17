460892e7f93684a5b52b0591715b619a48a35d16
Daniel Abrao <danielpaladar@gmail.com>
2021-06-17 01:19:02 -0300

    Filter brother name

--- client/main_module/src/js/factory/events.js
+++ client/main_module/src/js/factory/events.js
@@ -30,7 +30,7 @@ export default {
 		let url = new URL(`${API_ENDPOINT}/search`);
 
 		if (!filterColumn || filterColumn === "all") {
-			url.searchParams.append("extraFilterColumns", "EVENTO.CRIADO_EM,EVENTO.DESCRICAO,M.DATA_MINISTRADO,EVENTO.DATA_INICIO,EVENTO.DATA_FIM,M.TITULO");
+			url.searchParams.append("extraFilterColumns", "EVENTO.CRIADO_EM,EVENTO.DESCRICAO,M.DATA_MINISTRADO,EVENTO.DATA_INICIO,EVENTO.DATA_FIM,M.TITULO,I.IRMAO_NOME");
 		} else {
 			url.searchParams.append("filterColumn", filterColumn);
 		}
