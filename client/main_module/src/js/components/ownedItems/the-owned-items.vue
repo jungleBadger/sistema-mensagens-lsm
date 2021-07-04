f3010d773e6771eb5f3154db96045b41f7bdcf66
Daniel Abrao <danielpaladar@gmail.com>
2021-07-04 18:38:08 -0300

    Change item positioning

--- client/main_module/src/js/components/ownedItems/the-owned-items.vue
+++ client/main_module/src/js/components/ownedItems/the-owned-items.vue
@@ -30,8 +30,7 @@
 		>
 			<aside
 				style="z-index: 53;"
-				v-if="selectedAudioPath"
-				class="absolute flex w-full bg-white bottom-0 left-0 p-2 shadow items-center gap-2">
+				class="fixed flex w-full bg-white bottom-0 left-0 p-2 shadow items-center gap-2">
 				<audio
 					:key="selectedAudioPath"
 					class="h-10 flex-1"
