7571791ca7be672ed712dc095fc432737ee96efe
Daniel Abrao <danielpaladar@gmail.com>
2025-10-13 13:33:08 -0300

    Add tls.rejectUnauthorized option
    (cherry picked from commit 1d4a39caac43f65127693d02907e2cf79f73f652)

--- server/app/configs/mail-configs.js
+++ server/app/configs/mail-configs.js
@@ -4,9 +4,14 @@ module.exports = {
 	"service": process.env.EMAIL_SERVICE,
 	"host": process.env.EMAIL_HOST,
 	"port": process.env.EMAIL_PORT,
+	"secure": false,
 	"auth": {
 		"user": process.env.EMAIL_USER,
 		"pass": process.env.EMAIL_PASSWORD
+	},
+	"logger": true,
+	"debug": true,
+	"tls": {
+		"rejectUnauthorized": false
 	}
-
 };
